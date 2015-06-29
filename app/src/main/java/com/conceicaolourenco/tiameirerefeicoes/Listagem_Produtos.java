3276c03ff10606a2c9432c2adb694d97e57bde59
Conceição Lourenço <conceicaolourenzo@gmail.com>
2015-06-29 00:45:19 -0300

    Edicao de prato ok

--- app/src/main/java/com/conceicaolourenco/tiameirerefeicoes/Listagem_Produtos.java
+++ app/src/main/java/com/conceicaolourenco/tiameirerefeicoes/Listagem_Produtos.java
@@ -1,21 +1,27 @@
 package com.conceicaolourenco.tiameirerefeicoes;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.NavUtils;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 import SQLite.AcoesDataBase;
+import adapter.EditarProdutoAdapter;
 import adapter.ProdutoAdapter;
 import to.Produto;
 
@@ -23,6 +29,9 @@ import to.Produto;
  * Created by ConceicaoLourenco on 26/06/2015.
  */
 public class Listagem_Produtos extends Activity {
+
+    List<Produto> listaprodutos;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,34 +44,24 @@ public class Listagem_Produtos extends Activity {
 
         AcoesDataBase bd = new AcoesDataBase(this);
 
-        //List<Produto> list = bd.buscar();
-        //setListAdapter(new AdapterProduto(list, this));
-
-        List<Produto> lista = bd.buscar();
-
-        ProdutoAdapter adapter = new ProdutoAdapter(lista, this);
+        listaprodutos = bd.ListarProdutos();
+        ProdutoAdapter adapter = new ProdutoAdapter(listaprodutos, this);
 
         //PROCURA PELA LISTVIEW NO LAYTOU
-        final ListView listprod = (ListView) findViewById(R.id.listagem_produtos);
-        listprod.setAdapter(adapter);
+        final ListView listViewProd = (ListView) findViewById(R.id.listagem_produtos);
+        listViewProd.setAdapter(adapter);
 
    // -------------------------------------------------------------------------------------
         // ------- se clicar no item da lista ---------------
-        listprod.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listViewProd.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> list, View view, int position, long id) {
 
-                // ListView Clicked item index
-                int itemPosition = position;
-
-                // ListView Clicked item value
-                String  itemValue = (String) listprod.getItemAtPosition(position);
 
                 //---- CHamar o editar
-                EdicaoDEprato();
-                // Show Alert
-                //Toast.makeText(getApplicationContext(), "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG).show();
+                EdicaoDePrato();
+
 
             }
         });
@@ -70,22 +69,47 @@ public class Listagem_Produtos extends Activity {
         // -------------------------------------------------------------------------------------
     }
 
-    public void EdicaoDEprato(){
+    public void EdicaoDePrato(){
+
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View v = inflater.inflate(R.layout.editar_prato_activity, null);
+
+        EditText editCodigo = (EditText)v.findViewById(R.id.editCodigo);
+        EditText descricaoEt = (EditText)v.findViewById(R.id.editDescricao);
+        EditText precoEt = (EditText)v.findViewById(R.id.editPreco);
+
+        Toast.makeText(getApplicationContext(), "selecionado >  "
+                + descricaoEt.getText() + "Preco > "
+                + precoEt.getText() , Toast.LENGTH_LONG).show();
+
+    }
+
+
+    public void Editarprato(){
+       //TODO
+        /*
+        *
         Handler h = new Handler();
 
         h.postDelayed(new Runnable() {
             @Override
             public void run() {
                 //Intent i = new Intent(SplahActivity.this,ListaProdutoActivity.class);
-                Intent i = new Intent(Listagem_Produtos.this,Editar_Prato.class);
+                Intent i = new Intent(Listagem_Produtos.this,CustomDialogActivity.class);
                 startActivity(i);
                 finish();
             }
         },1500); // conta 2 seg
 
+        */
     }
 
-
+    public Produto EntrarNaBusca(String codigo) {
+        AcoesDataBase bd = new AcoesDataBase(this);
+        Produto produto = bd.buscar(codigo);
+        return produto;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
